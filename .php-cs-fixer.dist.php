ae84fbbe8a7a2869a5d9272ecfc5152a3ed421f7
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2022-11-29 18:36:30 +0800

    Drop support for PHP 7.4

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
 use Nexus\CsConfig\Factory;
 use Nexus\CsConfig\Fixer\Comment\NoCodeSeparatorCommentFixer;
 use Nexus\CsConfig\FixerGenerator;
-use Nexus\CsConfig\Ruleset\Nexus74;
+use Nexus\CsConfig\Ruleset\Nexus80;
 use PhpCsFixer\Finder;
 
 $finder = Finder::create()
@@ -24,7 +24,9 @@ $finder = Finder::create()
     ->append([__FILE__])
 ;
 
-$overrides = [];
+$overrides = [
+    'final_public_method_for_abstract_class' => false,
+];
 $options = [
     'finder' => $finder,
     'cacheFile' => 'build/.php-cs-fixer.cache',
@@ -34,4 +36,4 @@ $options = [
     ],
 ];
 
-return Factory::create(new Nexus74(), $overrides, $options)->forLibrary('Nexus CS Config', 'John Paul E. Balandan, CPA', 'paulbalandan@gmail.com', 2020);
+return Factory::create(new Nexus80(), $overrides, $options)->forLibrary('Nexus CS Config', 'John Paul E. Balandan, CPA', 'paulbalandan@gmail.com', 2020);
