334d05fbbf70395556551c8ef10cb7f06774f027
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2026-01-24 20:59:46 +0800

    Add new fixers of php-cs-fixer v3.93

--- src/Ruleset/Nexus81.php
+++ src/Ruleset/Nexus81.php
@@ -29,6 +29,7 @@ final class Nexus81 extends AbstractRuleset
             'array_push' => true,
             'array_syntax' => ['syntax' => 'short'],
             'assign_null_coalescing_to_coalesce_equal' => true,
+            'attribute_block_no_spaces' => true,
             'attribute_empty_parentheses' => ['use_parentheses' => false],
             'backtick_to_shell_exec' => true,
             'binary_operator_spaces' => [
@@ -232,6 +233,7 @@ final class Nexus81 extends AbstractRuleset
                 'attribute_placement' => 'standalone',
             ],
             'method_chaining_indentation' => true,
+            'modern_serialization_methods' => true,
             'modernize_strpos' => ['modernize_stripos' => true],
             'modernize_types_casting' => true,
             'modifier_keywords' => ['elements' => ['const', 'method', 'property']],
@@ -303,6 +305,7 @@ final class Nexus81 extends AbstractRuleset
             'no_multiple_statements_per_line' => true,
             'no_null_property_initialization' => true,
             'no_php4_constructor' => true,
+            'no_redundant_readonly_property' => true,
             'no_short_bool_cast' => true,
             'no_singleline_whitespace_before_semicolons' => true,
             'no_space_around_double_colon' => true,
@@ -581,6 +584,7 @@ final class Nexus81 extends AbstractRuleset
             'phpdoc_trim' => true,
             'phpdoc_trim_consecutive_blank_line_separation' => true,
             'phpdoc_types' => ['groups' => ['simple', 'alias', 'meta']],
+            'phpdoc_types_no_duplicates' => true,
             'phpdoc_types_order' => [
                 'null_adjustment' => 'always_first',
                 'sort_algorithm' => 'alpha',
