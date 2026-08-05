0ece7f876af8ce0fdb9a4e81b305f961c0a4201b
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2026-08-05 13:35:05 +0800

    Prune phpstan baseline via global ignores

--- phpstan-baseline.php
+++ phpstan-baseline.php
@@ -2,58 +2,10 @@
 
 $ignoreErrors = [];
 $ignoreErrors[] = [
-	'rawMessage' => 'Access to constant VERSION of internal class PhpCsFixer\\Console\\Application from outside its root namespace PhpCsFixer.',
-	'identifier' => 'classConstant.internalClass',
-	'count' => 1,
-	'path' => __DIR__ . '/src/Factory.php',
-];
-$ignoreErrors[] = [
 	'rawMessage' => 'Comparison operation ">" between int<80200, 80599> and 80599 is always false.',
 	'identifier' => 'greater.alwaysFalse',
 	'count' => 1,
 	'path' => __DIR__ . '/src/Ruleset/AbstractRuleset.php',
-];
-$ignoreErrors[] = [
-	'rawMessage' => 'Call to method __construct() of internal class PhpCsFixer\\FixerFactory from outside its root namespace PhpCsFixer.',
-	'identifier' => 'method.internalClass',
-	'count' => 1,
-	'path' => __DIR__ . '/src/Test/FixerProvider.php',
-];
-$ignoreErrors[] = [
-	'rawMessage' => 'Call to method __construct() of internal class PhpCsFixer\\RuleSet\\RuleSet from outside its root namespace PhpCsFixer.',
-	'identifier' => 'method.internalClass',
-	'count' => 1,
-	'path' => __DIR__ . '/src/Test/FixerProvider.php',
-];
-$ignoreErrors[] = [
-	'rawMessage' => 'Call to method getFixers() of internal class PhpCsFixer\\FixerFactory from outside its root namespace PhpCsFixer.',
-	'identifier' => 'method.internalClass',
-	'count' => 1,
-	'path' => __DIR__ . '/src/Test/FixerProvider.php',
-];
-$ignoreErrors[] = [
-	'rawMessage' => 'Call to method getRules() of internal class PhpCsFixer\\RuleSet\\RuleSet from outside its root namespace PhpCsFixer.',
-	'identifier' => 'method.internalClass',
-	'count' => 1,
-	'path' => __DIR__ . '/src/Test/FixerProvider.php',
-];
-$ignoreErrors[] = [
-	'rawMessage' => 'Call to method registerBuiltInFixers() of internal class PhpCsFixer\\FixerFactory from outside its root namespace PhpCsFixer.',
-	'identifier' => 'method.internalClass',
-	'count' => 1,
-	'path' => __DIR__ . '/src/Test/FixerProvider.php',
-];
-$ignoreErrors[] = [
-	'rawMessage' => 'Instantiation of internal class PhpCsFixer\\FixerFactory.',
-	'identifier' => 'new.internalClass',
-	'count' => 1,
-	'path' => __DIR__ . '/src/Test/FixerProvider.php',
-];
-$ignoreErrors[] = [
-	'rawMessage' => 'Instantiation of internal class PhpCsFixer\\RuleSet\\RuleSet.',
-	'identifier' => 'new.internalClass',
-	'count' => 1,
-	'path' => __DIR__ . '/src/Test/FixerProvider.php',
 ];
 
 return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
